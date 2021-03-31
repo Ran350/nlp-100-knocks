19fab543ee46e56dc4c8e1dd745e7f98afef9ad8
Hoshi Ran <ureshinostyle350@gmail.com>
2021-03-31 21:54:46 +0900

    問題12を追加

---
+++ section2/12.py
@@ -0,0 +1,7 @@
+from os import name
+import pandas as pd
+
+df = pd.read_table("./popular-names.txt", sep='\t', header=None)
+
+df[0].to_csv("./col1.txt", index=False, header=False)
+df[1].to_csv("./col2.txt", index=False, header=False)
